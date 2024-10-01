58878d371c01b467959614f81cccacc24dcdeff3
Thomas Schwarz <thosch@microsoft.com>
2024-10-01 10:54:06 -0500

    vnet address space modified

--- module/core/main.tf
+++ module/core/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "rg-automation" {
 
 resource "azurerm_virtual_network" "auto-acct-vnet" {
   name                = "example-vnet"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.0.0.0/22"]
   location            = var.global_settings.location
   resource_group_name = azurerm_resource_group.rg-automation.name
 }
